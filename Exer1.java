d072fd7ca5b3193626ee92d2d4283517536a64e4
carlosAugusto7 <carlosaugusto20003@gmail.com>
2025-02-20 20:50:03 -0300

    Create Exer1.java

---
+++ Exer1.java
@@ -0,0 +1,13 @@
+///Exercicio1
+import java.util.Scanner;
+
+public class SomaDoisNumeros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero1 = scanner.nextInt();
+        int numero2 = scanner.nextInt();
+        System.out.println(numero1 + numero2);
+        scanner.close();
+    }
+}
+
